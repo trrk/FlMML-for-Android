61ad9ec5a3e1e134184d96b30eda0de423348977
trrk <trrk@users.noreply.github.com>
2023-11-19 19:26:06 +0900

    kotlin 化

--- app/src/main/java/com/txt_nifty/sketch/flmml/MOscillator.kt
+++ app/src/main/java/com/txt_nifty/sketch/flmml/MOscillator.kt
@@ -1,110 +1,107 @@
-package com.txt_nifty.sketch.flmml;
+package com.txt_nifty.sketch.flmml
 
-public class MOscillator {
-    public static final int SINE = 0;
-    public static final int SAW = 1;
-    public static final int TRIANGLE = 2;
-    public static final int PULSE = 3;
-    public static final int NOISE = 4;
-    public static final int FC_PULSE = 5;
-    public static final int FC_TRI = 6;
-    public static final int FC_NOISE = 7;
-    public static final int FC_S_NOISE = 8;
-    public static final int FC_DPCM = 9;
-    public static final int GB_WAVE = 10;
-    public static final int GB_NOISE = 11;
-    public static final int GB_S_NOISE = 12;
-    public static final int WAVE = 13;
-    public static final int OPM = 14;
-    public static final int MAX = 15;
-    protected static int sInit = 0;
-    protected MOscMod[] mOsc;
-    protected int mForm;
+class MOscillator {
+    protected var mOsc: Array<MOscMod?>
+    var form = 0
+        protected set
 
-    public MOscillator() {
-        boot();
-        mOsc = new MOscMod[MAX];
-        mOsc[SINE] = new MOscSine();
-        mOsc[SAW] = new MOscSaw();
-        mOsc[TRIANGLE] = new MOscTriangle();
-        mOsc[PULSE] = new MOscPulse();
-        mOsc[NOISE] = new MOscNoise();
-        mOsc[FC_PULSE] = new MOscPulse();
-        mOsc[FC_TRI] = new MOscFcTri();
-        mOsc[FC_NOISE] = new MOscFcNoise();
-        mOsc[FC_S_NOISE] = null;
+    init {
+        boot()
+        mOsc = arrayOfNulls(MAX)
+        mOsc[SINE] = MOscSine()
+        mOsc[SAW] = MOscSaw()
+        mOsc[TRIANGLE] = MOscTriangle()
+        mOsc[PULSE] = MOscPulse()
+        mOsc[NOISE] = MOscNoise()
+        mOsc[FC_PULSE] = MOscPulse()
+        mOsc[FC_TRI] = MOscFcTri()
+        mOsc[FC_NOISE] = MOscFcNoise()
+        mOsc[FC_S_NOISE] = null
         //2009.05.10 OffGao MOD 1L addDPCM
         //mOsc[FC_DPCM]    = new MOscMod();
-        mOsc[FC_DPCM] = new MOscFcDpcm();
-        mOsc[GB_WAVE] = new MOscGbWave();
-        mOsc[GB_NOISE] = new MOscGbLNoise();
-        mOsc[GB_S_NOISE] = new MOscGbSNoise();
-        mOsc[WAVE] = new MOscWave();
-        mOsc[OPM] = new MOscOPM();
-        setForm(PULSE);
-        setNoiseToPulse();
+        mOsc[FC_DPCM] = MOscFcDpcm()
+        mOsc[GB_WAVE] = MOscGbWave()
+        mOsc[GB_NOISE] = MOscGbLNoise()
+        mOsc[GB_S_NOISE] = MOscGbSNoise()
+        mOsc[WAVE] = MOscWave()
+        mOsc[OPM] = MOscOPM()
+        setForm(PULSE)
+        setNoiseToPulse()
     }
 
-    public static void boot() {
-        if (sInit != 0) return;
-        MOscSine.boot();
-        MOscSaw.boot();
-        MOscTriangle.boot();
-        MOscPulse.boot();
-        MOscNoise.boot();
-        MOscFcTri.boot();
-        MOscFcNoise.boot();
-        //2009.05.10 OffGao ADD 1L addDPCM
-        MOscFcDpcm.boot();
-        MOscGbWave.boot();
-        MOscGbLNoise.boot();
-        MOscGbSNoise.boot();
-        MOscWave.boot();
-        MOscOPM.boot();
-        sInit = 1;
+    fun asLFO() {
+        if (mOsc[NOISE] != null) (mOsc[NOISE] as MOscNoise?)!!.disableResetPhase()
     }
 
-    public void asLFO() {
-        if (mOsc[NOISE] != null) ((MOscNoise) (mOsc[NOISE])).disableResetPhase();
-    }
-
-    public MOscMod setForm(int form) {
-        MOscNoise modNoise;
-        MOscFcNoise modFcNoise;
-        if (form >= MAX) form = MAX - 1;
-        mForm = form;
-        switch (form) {
-            case NOISE:
-                modNoise = (MOscNoise) (mOsc[NOISE]);
-                modNoise.restoreFreq();
-                break;
-            case FC_NOISE:
-                modFcNoise = (MOscFcNoise) (getMod(FC_NOISE));
-                modFcNoise.setLongMode();
-                break;
-            case FC_S_NOISE:
-                modFcNoise = (MOscFcNoise) (getMod(FC_S_NOISE));
-                modFcNoise.setShortMode();
-                break;
+    fun setForm(form: Int): MOscMod {
+        var form = form
+        if (form >= MAX) form = MAX - 1
+        this.form = form
+        when (form) {
+            NOISE -> {
+                val modNoise = mOsc[NOISE] as MOscNoise?
+                modNoise!!.restoreFreq()
+            }
+            FC_NOISE -> {
+                val modFcNoise = getMod(FC_NOISE) as MOscFcNoise?
+                modFcNoise!!.setLongMode()
+            }
+            FC_S_NOISE -> {
+                val modFcNoise = getMod(FC_S_NOISE) as MOscFcNoise?
+                modFcNoise!!.setShortMode()
+            }
         }
-        return getMod(form);
+        return getMod(form)
     }
 
-    public int getForm() {
-        return mForm;
+    val current: MOscMod
+        get() = getMod(form)
+
+    fun getMod(form: Int): MOscMod {
+        return if (form != FC_S_NOISE) mOsc[form]!! else mOsc[FC_NOISE]!!
     }
 
-    public MOscMod getCurrent() {
-        return getMod(mForm);
+    private fun setNoiseToPulse() {
+        val modPulse = getMod(PULSE) as MOscPulse?
+        val modNoise = getMod(NOISE) as MOscNoise?
+        modPulse!!.setNoise(modNoise)
     }
 
-    public MOscMod getMod(int form) {
-        return (form != FC_S_NOISE) ? mOsc[form] : mOsc[FC_NOISE];
-    }
-
-    private void setNoiseToPulse() {
-        MOscPulse modPulse = (MOscPulse) (getMod(PULSE));
-        MOscNoise modNoise = (MOscNoise) (getMod(NOISE));
-        modPulse.setNoise(modNoise);
+    companion object {
+        const val SINE = 0
+        const val SAW = 1
+        const val TRIANGLE = 2
+        const val PULSE = 3
+        const val NOISE = 4
+        const val FC_PULSE = 5
+        const val FC_TRI = 6
+        const val FC_NOISE = 7
+        const val FC_S_NOISE = 8
+        const val FC_DPCM = 9
+        const val GB_WAVE = 10
+        const val GB_NOISE = 11
+        const val GB_S_NOISE = 12
+        const val WAVE = 13
+        const val OPM = 14
+        const val MAX = 15
+        protected var sInit = 0
+        fun boot() {
+            if (sInit != 0) return
+            MOscSine.boot()
+            MOscSaw.boot()
+            MOscTriangle.boot()
+            MOscPulse.boot()
+            MOscNoise.boot()
+            MOscFcTri.boot()
+            MOscFcNoise.boot()
+            //2009.05.10 OffGao ADD 1L addDPCM
+            MOscFcDpcm.boot()
+            MOscGbWave.boot()
+            MOscGbLNoise.boot()
+            MOscGbSNoise.boot()
+            MOscWave.boot()
+            MOscOPM.boot()
+            sInit = 1
+        }
     }
 }
--- app/src/main/java/com/txt_nifty/sketch/flmml/rep/EventDispatcher.kt
+++ app/src/main/java/com/txt_nifty/sketch/flmml/rep/EventDispatcher.kt
@@ -1,21 +1,17 @@
-package com.txt_nifty.sketch.flmml.rep;
+package com.txt_nifty.sketch.flmml.rep
 
-import com.txt_nifty.sketch.flmml.MMLEvent;
+import com.txt_nifty.sketch.flmml.MMLEvent
 
-import java.util.ArrayList;
-
-public class EventDispatcher {
-
-    ArrayList<Object[]> listeners = new ArrayList<>();
-
-    public void addEventListener(String type, Callback listener) {
-        listeners.add(new Object[]{type, listener});
+open class EventDispatcher {
+    var listeners = ArrayList<Array<Any>>()
+    fun addEventListener(type: String, listener: Callback) {
+        listeners.add(arrayOf(type, listener))
     }
 
-    public void dispatchEvent(MMLEvent e) {
-        for (int i = 0; i < listeners.size(); i++) {
-            Object[] t = listeners.get(i);
-            if (t[0].equals(e.type)) ((Callback) t[1]).call(e);
+    fun dispatchEvent(e: MMLEvent) {
+        for (i in listeners.indices) {
+            val t = listeners[i]
+            if (t[0] == e.type) (t[1] as Callback).call(e)
         }
     }
 }
