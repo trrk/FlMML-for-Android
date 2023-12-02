ea9e2aa5627d6e4ccd285f2f34eded50b48421fb
trrk <trrk@users.noreply.github.com>
2023-12-02 19:25:38 +0900

    Kotlin 化

--- app/src/main/java/com/txt_nifty/sketch/flmml/MOscFcNoise.kt
+++ app/src/main/java/com/txt_nifty/sketch/flmml/MOscFcNoise.kt
@@ -1,125 +1,127 @@
-package com.txt_nifty.sketch.flmml;
+package com.txt_nifty.sketch.flmml
 
 /**
  * Special thanks to OffGao.
  */
+class MOscFcNoise : MOscMod() {
+    protected var mFcr: Int
+    protected var mSnz = 0
+    protected var mVal: Double
 
-public class MOscFcNoise extends MOscMod {
-    public static final int FC_NOISE_PHASE_SFT = 10;
-    public static final int FC_NOISE_PHASE_SEC = (1789773 << FC_NOISE_PHASE_SFT);
-    public static final int FC_NOISE_PHASE_DLT = FC_NOISE_PHASE_SEC / 44100;
-    protected static int[] s_interval = new int[]{0x004, 0x008, 0x010, 0x020, 0x040, 0x060, 0x080, 0x0a0,
-            0x0ca, 0x0fe, 0x17c, 0x1fc, 0x2fa, 0x3f8, 0x7f2, 0xfe4};
-    protected int mFcr;
-    protected int mSnz;
-    protected double mVal;
-
-    public MOscFcNoise() {
-        boot();
-        super_init();
-        setLongMode();
-        mFcr = 0x8000;
-        mVal = getValue();
-        setNoiseFreq(0);
+    init {
+        boot()
+        super_init()
+        setLongMode()
+        mFcr = 0x8000
+        mVal = getValue()
+        setNoiseFreq(0)
     }
 
-    public static void boot() {
+    private fun getValue(): Double {
+        mFcr = mFcr shr 1
+        mFcr = mFcr or (((mFcr xor (mFcr shr mSnz)) and 1) shl 15)
+        return if (mFcr and 1 != 0) 1.0 else -1.0
     }
 
-    private double getValue() {
-        mFcr >>= 1;
-        mFcr |= ((mFcr ^ (mFcr >> mSnz)) & 1) << 15;
-        return (mFcr & 1) != 0 ? 1.0 : -1.0;
+    fun setShortMode() {
+        mSnz = 6
     }
 
-    public void setShortMode() {
-        mSnz = 6;
+    fun setLongMode() {
+        mSnz = 1
     }
 
-    public void setLongMode() {
-        mSnz = 1;
-    }
-
-    public void resetPhase() {
-    }
-
-    public void addPhase(int time) {
-        mPhase = mPhase + FC_NOISE_PHASE_DLT * time;
+    override fun resetPhase() {}
+    override fun addPhase(time: Int) {
+        mPhase = mPhase + FC_NOISE_PHASE_DLT * time
         while (mPhase >= mFreqShift) {
-            mPhase -= mFreqShift;
-            mVal = getValue();
+            mPhase -= mFreqShift
+            mVal = getValue()
         }
     }
 
-    public double getNextSample() {
-        double val = mVal;
-        double sum = 0;
-        double cnt = 0;
-        int delta = FC_NOISE_PHASE_DLT;
+    override fun getNextSample(): Double {
+        val `val` = mVal
+        var sum = 0.0
+        var cnt = 0.0
+        var delta = FC_NOISE_PHASE_DLT
         while (delta >= mFreqShift) {
-            delta -= mFreqShift;
-            mPhase = 0;
-            sum += getValue();
-            cnt += 1.0;
+            delta -= mFreqShift
+            mPhase = 0
+            sum += getValue()
+            cnt += 1.0
         }
         if (cnt > 0) {
-            mVal = sum / cnt;
+            mVal = sum / cnt
         }
-        mPhase += delta;
+        mPhase += delta
         if (mPhase >= mFreqShift) {
-            mPhase -= mFreqShift;
-            mVal = getValue();
+            mPhase -= mFreqShift
+            mVal = getValue()
         }
-        return val;
+        return `val`
     }
 
-    public double getNextSampleOfs(int ofs) {
-        int fcr = mFcr;
-        int phase = mPhase;
-        double val = mVal;
-        double sum = 0;
-        double cnt = 0;
-        int delta = FC_NOISE_PHASE_DLT + ofs;
+    override fun getNextSampleOfs(ofs: Int): Double {
+        val fcr = mFcr
+        val phase = mPhase
+        val `val` = mVal
+        var sum = 0.0
+        var cnt = 0.0
+        var delta = FC_NOISE_PHASE_DLT + ofs
         while (delta >= mFreqShift) {
-            delta -= mFreqShift;
-            mPhase = 0;
-            sum += getValue();
-            cnt += 1.0;
+            delta -= mFreqShift
+            mPhase = 0
+            sum += getValue()
+            cnt += 1.0
         }
         if (cnt > 0) {
-            mVal = sum / cnt;
+            mVal = sum / cnt
         }
-        mPhase += delta;
+        mPhase += delta
         if (mPhase >= mFreqShift) {
-            mPhase = mFreqShift;
-            mVal = getValue();
+            mPhase = mFreqShift
+            mVal = getValue()
         }
-            /* */
-        mFcr = fcr;
-        mPhase = phase;
-        getNextSample();
-        return val;
+        /* */
+        mFcr = fcr
+        mPhase = phase
+        getNextSample()
+        return `val`
     }
 
-    public void getSamples(double[] samples, int start, int end) {
-        double val;
-        for (int i = start; i < end; i++) {
-            samples[i] = getNextSample();
+    override fun getSamples(samples: DoubleArray, start: Int, end: Int) {
+        var `val`: Double
+        for (i in start until end) {
+            samples[i] = getNextSample()
         }
     }
 
-    public void setFrequency(double frequency) {
+    override fun setFrequency(frequency: Double) {
         //m_frequency = frequency;
-        mFreqShift = (int) (FC_NOISE_PHASE_SEC / frequency);
+        mFreqShift = (FC_NOISE_PHASE_SEC / frequency).toInt()
     }
 
-    public void setNoiseFreq(int no) {
-        if (no < 0) no = 0;
-        if (no > 15) no = 15;
-        mFreqShift = s_interval[no] << FC_NOISE_PHASE_SFT; // as interval
+    fun setNoiseFreq(no: Int) {
+        var no = no
+        if (no < 0) no = 0
+        if (no > 15) no = 15
+        mFreqShift = s_interval[no] shl FC_NOISE_PHASE_SFT // as interval
     }
 
-    public void setNoteNo(int noteNo) {
-        setNoiseFreq(noteNo);
+    override fun setNoteNo(noteNo: Int) {
+        setNoiseFreq(noteNo)
+    }
+
+    companion object {
+        const val FC_NOISE_PHASE_SFT = 10
+        const val FC_NOISE_PHASE_SEC = 1789773 shl FC_NOISE_PHASE_SFT
+        const val FC_NOISE_PHASE_DLT = FC_NOISE_PHASE_SEC / 44100
+        protected var s_interval = intArrayOf(
+            0x004, 0x008, 0x010, 0x020, 0x040, 0x060, 0x080, 0x0a0,
+            0x0ca, 0x0fe, 0x17c, 0x1fc, 0x2fa, 0x3f8, 0x7f2, 0xfe4
+        )
+
+        fun boot() {}
     }
 }
--- app/src/main/java/com/txt_nifty/sketch/flmml/MOscFcTri.kt
+++ app/src/main/java/com/txt_nifty/sketch/flmml/MOscFcTri.kt
@@ -1,71 +1,85 @@
-package com.txt_nifty.sketch.flmml;
+package com.txt_nifty.sketch.flmml
 
-public class MOscFcTri extends MOscMod {
-    public static final int FC_TRI_TABLE_LEN = (1 << 5);
-    public static final int MAX_WAVE = 2;
-    protected static int sInit = 0;
-    protected static double[][] sTable;
-    protected int mWaveNo;
+class MOscFcTri : MOscMod() {
+    protected var mWaveNo = 0
 
-    public MOscFcTri() {
-        boot();
-        super_init();
-        setWaveNo(0);
+    init {
+        boot()
+        super_init()
+        setWaveNo(0)
     }
 
-    public static void boot() {
-        if (sInit != 0) return;
-        sTable = new double[MAX_WAVE][];
-        sTable[0] = new double[FC_TRI_TABLE_LEN];    // @6-0
-        sTable[1] = new double[FC_TRI_TABLE_LEN];    // @6-1
-        for (int i = 0; i < 16; i++) {
-            sTable[0][i] = sTable[0][31 - i] = i * 2.0 / 15.0 - 1.0;
-        }
-        for (int i = 0; i < 32; i++) {
-            sTable[1][i] = (i < 8) ? i * 2.0 / 14.0 : ((i < 24) ? (8 - i) * 2.0 / 15.0 + 1.0 : (i - 24) * 2.0 / 15.0 - 1.0);
-        }
-        sInit = 1;
+    override fun getNextSample(): Double {
+        val `val` = sTable[mWaveNo][mPhase shr (PHASE_SFT + 11)]
+        mPhase = (mPhase + mFreqShift) and PHASE_MSK
+        return `val`
     }
 
-    public double getNextSample() {
-        double val = sTable[mWaveNo][mPhase >> (PHASE_SFT + 11)];
-        mPhase = (mPhase + mFreqShift) & PHASE_MSK;
-        return val;
+    override fun getNextSampleOfs(ofs: Int): Double {
+        val `val` = sTable[mWaveNo][((mPhase + ofs) and PHASE_MSK) shr (PHASE_SFT + 11)]
+        mPhase = (mPhase + mFreqShift) and PHASE_MSK
+        return `val`
     }
 
-    public double getNextSampleOfs(int ofs) {
-        double val = sTable[mWaveNo][((mPhase + ofs) & PHASE_MSK) >> (PHASE_SFT + 11)];
-        mPhase = (mPhase + mFreqShift) & PHASE_MSK;
-        return val;
-    }
-
-    public void getSamples(double[] samples, int start, int end) {
-        for (int i = start; i < end; i++) {
-            samples[i] = sTable[mWaveNo][mPhase >> (PHASE_SFT + 11)];
-            mPhase = (mPhase + mFreqShift) & PHASE_MSK;
+    override fun getSamples(samples: DoubleArray, start: Int, end: Int) {
+        for (i in start until end) {
+            samples[i] = sTable[mWaveNo][mPhase shr (PHASE_SFT + 11)]
+            mPhase = (mPhase + mFreqShift) and PHASE_MSK
         }
     }
 
-    public void getSamplesWithSyncIn(double[] samples, boolean[] syncin, int start, int end) {
-        for (int i = start; i < end; i++) {
+    override fun getSamplesWithSyncIn(
+        samples: DoubleArray,
+        syncin: BooleanArray,
+        start: Int,
+        end: Int
+    ) {
+        for (i in start until end) {
             if (syncin[i]) {
-                resetPhase();
+                resetPhase()
             }
-            samples[i] = sTable[mWaveNo][mPhase >> (PHASE_SFT + 11)];
-            mPhase = (mPhase + mFreqShift) & PHASE_MSK;
+            samples[i] = sTable[mWaveNo][mPhase shr (PHASE_SFT + 11)]
+            mPhase = (mPhase + mFreqShift) and PHASE_MSK
         }
     }
 
-    public void getSamplesWithSyncOut(double[] samples, boolean[] syncout, int start, int end) {
-        for (int i = start; i < end; i++) {
-            samples[i] = sTable[mWaveNo][mPhase >> (PHASE_SFT + 11)];
-            mPhase += mFreqShift;
-            syncout[i] = (mPhase > PHASE_MSK);
-            mPhase &= PHASE_MSK;
+    override fun getSamplesWithSyncOut(
+        samples: DoubleArray,
+        syncout: BooleanArray,
+        start: Int,
+        end: Int
+    ) {
+        for (i in start until end) {
+            samples[i] = sTable[mWaveNo][mPhase shr (PHASE_SFT + 11)]
+            mPhase += mFreqShift
+            syncout[i] = mPhase > PHASE_MSK
+            mPhase = mPhase and PHASE_MSK
         }
     }
 
-    public void setWaveNo(int waveNo) {
-        mWaveNo = Math.min(waveNo, MAX_WAVE - 1);
+    override fun setWaveNo(waveNo: Int) {
+        mWaveNo = Math.min(waveNo, MAX_WAVE - 1)
+    }
+
+    companion object {
+        const val FC_TRI_TABLE_LEN = 1 shl 5
+        const val MAX_WAVE = 2
+        protected var sInit = 0
+        protected lateinit var sTable: Array<DoubleArray>
+        fun boot() {
+            if (sInit != 0) return
+            // @6-0 @6-1
+            sTable = Array(MAX_WAVE) { DoubleArray(FC_TRI_TABLE_LEN) }
+            for (i in 0..15) {
+                val v = i * 2.0 / 15.0 - 1.0
+                sTable[0][31 - i] = v
+                sTable[0][i] = v
+            }
+            for (i in 0..31) {
+                sTable[1][i] =
+                    if (i < 8) i * 2.0 / 14.0 else if (i < 24) (8 - i) * 2.0 / 15.0 + 1.0 else (i - 24) * 2.0 / 15.0 - 1.0
+            }
+            sInit = 1
+        }
     }
 }
