1ca1a8f53e3e469cfda9e97ac497d9ed511a2209
trrk <trrk@users.noreply.github.com>
2023-12-16 15:35:36 +0900

    Kotlin 化

--- app/src/main/java/com/txt_nifty/sketch/flmml/MOscNoise.kt
+++ app/src/main/java/com/txt_nifty/sketch/flmml/MOscNoise.kt
@@ -1,84 +1,84 @@
-package com.txt_nifty.sketch.flmml;
+package com.txt_nifty.sketch.flmml
 
+class MOscNoise : MOscMod() {
+    protected var mNoiseFreq = 0.0
+    protected var mCounter: Long
+    protected var mResetPhase: Boolean
 
-public class MOscNoise extends MOscMod {
-    public static final int TABLE_MSK = TABLE_LEN - 1;
-    public static final int NOISE_PHASE_SFT = 30;
-    public static final int NOISE_PHASE_MSK = (1 << NOISE_PHASE_SFT) - 1;
-    protected static int sInit = 0;
-    protected static double[] sTable = new double[TABLE_LEN];
-    protected double mNoiseFreq;
-    protected long mCounter;
-    protected boolean mResetPhase;
-
-    public MOscNoise() {
-        boot();
-        super_init();
-        setNoiseFreq(1.0);
-        mPhase = 0;
-        mCounter = 0;
-        mResetPhase = true;
+    init {
+        boot()
+        super_init()
+        setNoiseFreq(1.0)
+        mPhase = 0
+        mCounter = 0
+        mResetPhase = true
     }
 
-    public static void boot() {
-        if (sInit != 0) return;
-        for (int i = 0; i < TABLE_LEN; i++) {
-            sTable[i] = Math.random() * 2.0 - 1.0;
-        }
-        sInit = 1;
+    fun disableResetPhase() {
+        mResetPhase = false
     }
 
-    public void disableResetPhase() {
-        mResetPhase = false;
-    }
-
-    public void resetPhase() {
-        if (mResetPhase) mPhase = 0;
+    override fun resetPhase() {
+        if (mResetPhase) mPhase = 0
         //mCounter = 0;
     }
 
-    public void addPhase(int time) {
-        mCounter = (mCounter + mFreqShift * time);
-        mPhase = (int) ((mPhase + (mCounter >> NOISE_PHASE_SFT)) & TABLE_MSK);
-        mCounter &= NOISE_PHASE_MSK;
+    override fun addPhase(time: Int) {
+        mCounter = mCounter + mFreqShift * time
+        mPhase = ((mPhase + (mCounter shr NOISE_PHASE_SFT)) and TABLE_MSK.toLong()).toInt()
+        mCounter = mCounter and NOISE_PHASE_MSK.toLong()
     }
 
-    public double getNextSample() {
-        double val = sTable[mPhase];
-        mCounter = (mCounter + mFreqShift);
-        mPhase = (int) ((mPhase + (mCounter >> NOISE_PHASE_SFT)) & TABLE_MSK);
-        mCounter &= NOISE_PHASE_MSK;
-        return val;
+    override fun getNextSample(): Double {
+        val `val` = sTable[mPhase]
+        mCounter = mCounter + mFreqShift
+        mPhase = ((mPhase + (mCounter shr NOISE_PHASE_SFT)) and TABLE_MSK.toLong()).toInt()
+        mCounter = mCounter and NOISE_PHASE_MSK.toLong()
+        return `val`
     }
 
-    public double getNextSampleOfs(int ofs) {
-        double val = sTable[(mPhase + (ofs << PHASE_SFT)) & TABLE_MSK];
-        mCounter = (mCounter + mFreqShift);
-        mPhase = (int) ((mPhase + (mCounter >> NOISE_PHASE_SFT)) & TABLE_MSK);
-        mCounter &= NOISE_PHASE_MSK;
-        return val;
+    override fun getNextSampleOfs(ofs: Int): Double {
+        val `val` = sTable[(mPhase + (ofs shl PHASE_SFT)) and TABLE_MSK]
+        mCounter = mCounter + mFreqShift
+        mPhase = ((mPhase + (mCounter shr NOISE_PHASE_SFT)) and TABLE_MSK.toLong()).toInt()
+        mCounter = mCounter and NOISE_PHASE_MSK.toLong()
+        return `val`
     }
 
-    public void getSamples(double[] samples, int start, int end) {
-        int i;
-        for (i = start; i < end; i++) {
-            samples[i] = sTable[mPhase];
-            mCounter = (mCounter + mFreqShift);
-            mPhase = (int) ((mPhase + (mCounter >> NOISE_PHASE_SFT)) & TABLE_MSK);
-            mCounter &= NOISE_PHASE_MSK;
+    override fun getSamples(samples: DoubleArray, start: Int, end: Int) {
+        for (i in start until end) {
+            samples[i] = sTable[mPhase]
+            mCounter = mCounter + mFreqShift
+            mPhase = ((mPhase + (mCounter shr NOISE_PHASE_SFT)) and TABLE_MSK.toLong()).toInt()
+            mCounter = mCounter and NOISE_PHASE_MSK.toLong()
         }
     }
 
-    public void setFrequency(double frequency) {
-        mFrequency = frequency;
+    override fun setFrequency(frequency: Double) {
+        mFrequency = frequency
     }
 
-    public void setNoiseFreq(double frequency) {
-        mNoiseFreq = frequency * (1 << NOISE_PHASE_SFT);
-        mFreqShift = (int) mNoiseFreq;
+    fun setNoiseFreq(frequency: Double) {
+        mNoiseFreq = frequency * (1 shl NOISE_PHASE_SFT)
+        mFreqShift = mNoiseFreq.toInt()
     }
 
-    public void restoreFreq() {
-        mFreqShift = (int) mNoiseFreq;
+    fun restoreFreq() {
+        mFreqShift = mNoiseFreq.toInt()
+    }
+
+    companion object {
+        const val TABLE_MSK = TABLE_LEN - 1
+        const val NOISE_PHASE_SFT = 30
+        const val NOISE_PHASE_MSK = (1 shl NOISE_PHASE_SFT) - 1
+        protected var sInit = 0
+        protected var sTable = DoubleArray(TABLE_LEN)
+        fun boot() {
+            if (sInit != 0) return
+            for (i in 0 until TABLE_LEN) {
+                sTable[i] = Math.random() * 2.0 - 1.0
+            }
+            sInit = 1
+        }
     }
 }
